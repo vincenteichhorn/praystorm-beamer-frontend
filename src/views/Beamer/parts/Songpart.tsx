fee9e01d2c5cec987f2a176bf283302159b02193
Juliussssssssss <j.dachsel@freenet.de>
2020-03-19 23:18:45 +0100

    perspective + text shadow

--- src/views/Beamer/parts/Songpart.tsx
+++ src/views/Beamer/parts/Songpart.tsx
@@ -24,54 +24,68 @@ const Songpart: FunctionComponent<Props> = (props) => {
 
   return (
     <Box
-      id="beamer"
       style={{
-        transform: `rotateX(${beamerStore.adjustment.rotateX}deg) rotateY(${beamerStore.adjustment.rotateY}deg) scale(${beamerStore.adjustment.scale}, ${beamerStore.adjustment.scale})`,
-        backgroundImage: (backgroundImageExists) ? 'url(' + slide.data.style.backgroundImage + ')' : 'none',
-        backgroundColor: (backgroundImageExists) ? '' : slide.data.style.backgroundColor,
-        backgroundRepeat: (backgroundImageExists) ? 'no-repeat' : '',
-        backgroundAttachment: (backgroundImageExists) ? 'fixed' : '',
-        backgroundPosition: (backgroundImageExists) ? 'left top' : '',
-        backgroundSize: (backgroundImageExists) ? 'cover' : '',
-
-        textAlign: 'center',
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
+        perspective: '1079px',
+        height: '100vh',
       }}
     >
       <Box
+        id="beamer"
         style={{
-        }}
-      >
-        {
-          slide.data.lyrics.map((verse: string, index: number) => (
-            <Box 
-              key={index}
-              fontSize={slide.data.style.verseFontSize}
-              style={{
-                marginBottom: slide.data.style.verseSpacing,
-                color: slide.data.style.verseColor,
-              }}
-            >
-              {verse}
-            </Box>
-          ))
-        }
-      </Box>
-      <Box
-        style={{
-          textAlign: 'left',
-          color: slide.data.style.copyrightColor,
-          fontSize: slide.data.style.copyrightFontSize,
+          transform: `rotateX(${beamerStore.adjustment.rotateX}deg) rotateY(${beamerStore.adjustment.rotateY}deg) scale(${beamerStore.adjustment.scale}, ${beamerStore.adjustment.scale})`,
+          backgroundImage: (backgroundImageExists) ? 'url(' + slide.data.style.backgroundImage + ')' : 'none',
+          backgroundColor: (backgroundImageExists) ? '' : slide.data.style.backgroundColor,
+          backgroundRepeat: (backgroundImageExists) ? 'no-repeat' : '',
+          backgroundAttachment: (backgroundImageExists) ? 'fixed' : '',
+          backgroundPosition: (backgroundImageExists) ? 'left top' : '',
+          backgroundSize: (backgroundImageExists) ? 'cover' : '',
+
+          textAlign: 'center',
           position: 'absolute',
-          bottom: 15,
-          left: 15,
+          width: '100%',
+          height: '100%',
+          transformStyle: 'preserve-3d',
         }}
       >
-        <Typography>{slide.copyright.author}</Typography>
-        <Typography>{slide.copyright.album}</Typography>
-        <Typography>{slide.copyright.copyright}</Typography>
+        <Box
+          style={{
+            display: 'flex',
+            height: '95%',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          {
+            slide.data.lyrics.map((verse: string, index: number) => (
+              <Box 
+                key={index}
+                fontSize={slide.data.style.verseFontSize}
+                style={{
+                  marginBottom: slide.data.style.verseSpacing,
+                  color: slide.data.style.verseColor,
+                  textShadow: '1px 1px 2px black',
+                }}
+              >
+                {verse}
+              </Box>
+            ))
+          }
+        </Box>
+        <Box
+          style={{
+            textAlign: 'left',
+            color: slide.data.style.copyrightColor,
+            fontSize: slide.data.style.copyrightFontSize,
+            position: 'absolute',
+            bottom: 15,
+            left: 15,
+          }}
+        >
+          <Typography>{slide.copyright.author}</Typography>
+          <Typography>{slide.copyright.album}</Typography>
+          <Typography>{slide.copyright.copyright}</Typography>
+        </Box>
       </Box>
     </Box>
   );

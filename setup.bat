4f9fa72cada080014ea2e5709acf9cb98e40ca13
Vincent Eichhorn <vincent.cuvei@gmail.com>
2020-10-17 17:42:05 +0200

    changed setup.bat with pc-name usage

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
-FOR /F "delims=: tokens=2" %%a in ('ipconfig ^| find "IPv4"') do set _IPAddress=%%a
-set _IPAddress=%_IPAddress:~1%
+FOR /F "delims=: tokens=2" %%a in ('systeminfo ^| find "Hostname"') do set _IPAddress=%%a
+set _IPAddress=%_IPAddress:~38%
 ECHO REACT_APP_API_HOST=http://%_IPAddress%/praystorm-beamer-backend/api > "praystorm-beamer-frontend\.env"
 ECHO REACT_APP_SOCKET_HOST=http://%_IPAddress%:4001 >> "praystorm-beamer-frontend\.env"
 xampp\xampp_start.exe
